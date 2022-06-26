20d29f95424ef9741588171914f9c7ccbc36f6a6
Mahmoud Elkordy <mahmoud_elkordy65@yahoo.com>
2022-06-26 21:19:55 +0200

    image swap added in team section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let navbar = document.querySelector('.navbar');
 let burgerIcon = document.querySelector('.menu-btn');
 let scrollbtn = document.querySelector('.scroll-up-btn');
+let cardsSection = document.querySelector('.team-content');
+let currentCard = 1;
+let cardsArray = Array.from(cardsSection.children);
+let cardsCount = cardsArray.length;
 
 // toggle background-color of navbar
 window.onscroll = () => {
@@ -16,6 +20,18 @@ window.onscroll = () => {
     }
 };
 
+checker();
+
+let counter = setInterval(imageSwap, 1000);
+
+cardsSection.onmouseover = () => {
+    clearInterval(counter);
+}
+
+cardsSection.onmouseout = () => {
+    counter = setInterval(imageSwap, 1000);
+}
+
 // toggle menu/navbar script
 burgerIcon.onclick = () => {
     document.querySelector('.navbar .menu').classList.toggle('active');
@@ -27,4 +43,21 @@ scrollbtn.onclick = () => {
         top: 0,
         behavior: 'smooth'
     });
+}
+
+function checker() {
+    removeActive();
+    cardsArray[ currentCard-1 ].classList.add('active');
+}
+
+function removeActive() {
+    for(let i=0; i<cardsCount; i++) {
+        cardsArray[i].classList.remove('active');
+    }
+}
+
+function imageSwap() {
+    checker();
+    currentCard++;
+    if(currentCard === cardsCount+1) currentCard = 1;
 }
